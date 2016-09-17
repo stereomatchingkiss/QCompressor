ca8af5ceae39aa676d9f1c755e73d2982ef3f12e
stereomatchingkiss <stereomatchingkiss@gmail.com>
2016-09-17 12:42:39 +0800

    support file/folder exclusion when compress folder

--- main.cpp
+++ main.cpp
@@ -52,14 +52,16 @@ int main(int argc, char *argv[])
 }
 
 void compress(QString const &file_to_compress,
-              QString const &compress_as);
+              QString const &compress_as,
+              QStringList const &exclude);
 
 void decompress(QString const &compress_type,
                 QString const &file_to_decompress,
                 QString const &decompress_as);
 
 void compress(QString const &file_to_compress,
-              QString const &compress_as)
+              QString const &compress_as,
+              QStringList const &exclude)
 {
     QFileInfo file_info(file_to_compress);
     if(file_info.isFile()){
@@ -73,7 +75,7 @@ void compress(QString const &file_to_compress,
         qDebug()<<"compress "<<file_info.fileName()
                <<" to "<<QFileInfo(compress_as).fileName();
         qte::cp::folder_compressor fc;
-        if(!fc.compress_folder(file_to_compress, compress_as)){
+        if(!fc.compress_folder(file_to_compress, compress_as, exclude)){
             qDebug()<<"cannot compress "<<file_info.fileName()
                    <<" to "<<QFileInfo(compress_as).fileName();
         }
@@ -86,24 +88,26 @@ void compress_decompress(QString const &file_location, bool is_compress = true)
     if(file.open(QIODevice::ReadOnly)){
         QTextStream stream(&file);
         auto line = stream.readLine();
-        while(!line.isEmpty()){
-            QTextStream read_line(&line, QIODevice::ReadOnly);
+        while(!line.isEmpty()){            
+            auto const split_str = line.split("\t");
             QString compress_type;
             QString input_file;
             QString output_as;
-            if(is_compress){
-                read_line>>input_file>>output_as;
-            }else{
-                read_line>>compress_type>>input_file>>output_as;
-            }
-            if(read_line.status() == QTextStream::Ok){
-                if(is_compress){
-                    compress(input_file, output_as);
+            if(is_compress && split_str.size() >= 2){
+                input_file = split_str[0];
+                output_as = split_str[1];
+                if(split_str.size() == 2){
+                    compress(input_file, output_as, {});
                 }else{
-                    decompress(compress_type, input_file, output_as);
+                    compress(input_file, output_as, split_str.mid(2));
                 }
+            }else if(split_str.size() >= 3){
+                compress_type = split_str[0];
+                input_file = split_str[1];
+                output_as = split_str[2];
+                decompress(compress_type, input_file, output_as);
             }else{
-                qDebug()<<"fail to parse line : "<<line;
+                qDebug()<<"fail to parse compress file or decompress file";
             }
             line = stream.readLine();
         }
